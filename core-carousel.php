0ce9f3e3a9dafaf0e2d1ced787506dabeafda867
Masud Rana <mr.masudrana00@gmail.com>
2026-02-02 05:14:29 +0600

    refactor: Update version numbers to 1.0.0 across all relevant files

--- core-carousel.php
+++ core-carousel.php
@@ -7,7 +7,7 @@
  * Author URI:  https://rtcamp.com
  * License:     GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Version:     2.0.0
+ * Version:     1.0.0
  * Text Domain: core-carousel
  *
  * @package core-carousel

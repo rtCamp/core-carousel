39fef4b23c45d031889be83efe49b4386c3c7ed6
Danish Shakeel <danishshakeel54@gmail.com>
2026-07-19 14:20:46 +0200

    feat: add tabpanel role and ARIA bindings to slide save in tabs mode

--- src/blocks/carousel/slide/save.tsx
+++ src/blocks/carousel/slide/save.tsx
@@ -1,17 +1,33 @@
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 import type { CarouselSlideAttributes } from '../types';
 
-export default function Save( { attributes }: { attributes: CarouselSlideAttributes } ) {
+export default function Save( {
+	attributes,
+	context,
+}: {
+	attributes: CarouselSlideAttributes;
+	context?: { 'rt-carousel/carousel/useTabs'?: boolean };
+} ) {
 	const { verticalAlignment } = attributes;
+	const useTabs = context?.[ 'rt-carousel/carousel/useTabs' ] ?? false;
+
 	const blockProps = useBlockProps.save( {
 		className: `embla__slide${
 			verticalAlignment ? ` is-vertically-aligned-${ verticalAlignment }` : ''
 		}`,
-		role: 'group',
-		'aria-roledescription': 'slide',
+		role: useTabs ? 'tabpanel' : 'group',
+		...( ! useTabs ? { 'aria-roledescription': 'slide' } : {} ),
 		'data-wp-interactive': 'rt-carousel/carousel',
-		'data-wp-class--is-active': 'callbacks.isSlideActive',
-		'data-wp-bind--aria-current': 'callbacks.isSlideActive',
+		...( useTabs
+			? {
+				'data-wp-bind--id': 'callbacks.getSlideTabPanelId',
+				'data-wp-bind--aria-labelledby': 'callbacks.getSlideTabLabelledBy',
+				'data-wp-bind--hidden': 'callbacks.isSlideHiddenForTabs',
+			}
+			: {
+				'data-wp-class--is-active': 'callbacks.isSlideActive',
+				'data-wp-bind--aria-current': 'callbacks.isSlideActive',
+			} ),
 	} );
 
 	const innerBlocksProps = useInnerBlocksProps.save( blockProps );
